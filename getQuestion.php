129715a3b68247e8e7e96e3bb04598cd56772a9b
Axel Maczkowiak <axel.maczkowiak@live.fr>
2019-05-23 14:38:52 +0200

    GetQuestion First Impl

--- getQuestion.php
+++ getQuestion.php
@@ -3,7 +3,20 @@
 include_once 'connect.php';
 
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
 
-if (iss)
+if (issest($_GET['id']) || !empty($_GET['id'])){
+    $id = htmlentities($_GET['id']);
+    if ($id > 0 && $id < 23) {
+        $query = "SELECT * FROM questions WHERE question_id = ".$id.";";
+        $res = mysqli_query($Connect,$query);
+        $data = mysqli_fetch_row ($res);
+        echo json_encode($data);
+    } else {
+        http_response_code(404);
+    }
+} else {
+    http_response_code(400);
+}
 
 ?>
