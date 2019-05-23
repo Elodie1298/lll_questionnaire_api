485c4f72e8d37ae2b3078289bcaca9d10150e79a
Axel Maczkowiak <axel.maczkowiak@live.fr>
2019-05-23 14:18:35 +0200

    Fixing CORS

--- addPerson.php
+++ addPerson.php
@@ -1,18 +1,19 @@
 <?php
-    include_once 'connect.php';
 
-    header("Access-Control-Allow-Origin: *");
-    
-    $age = htmlspecialchars($_GET["age"]);
-    $gender = htmlspecialchars($_GET["gender"]);
-    $activity = htmlspecialchars($_GET["activity"]);
+include_once 'connect.php';
 
-    $query = "INSERT INTO user (user_gender, user_age, user_activity) VALUES ('".$gender."', '".$age."', '".$activity."')";
-    $Connect->query($query);
+header("Access-Control-Allow-Origin: *");
 
-	$query = "SELECT user_id FROM user ORDER BY user_id DESC LIMIT 1;";
-	$_SESSION['user_id'] = $Connect->query($query);
+$age = htmlspecialchars($_GET["age"]);
+$gender = htmlspecialchars($_GET["gender"]);
+$activity = htmlspecialchars($_GET["activity"]);
 
+$query = "INSERT INTO user (user_gender, user_age, user_activity) VALUES ('".$gender."', '".$age."', '".$activity."')";
+$Connect->query($query);
 
-	http_response_code(200);
+$query = "SELECT user_id FROM user ORDER BY user_id DESC LIMIT 1;";
+$_SESSION['user_id'] = $Connect->query($query);
+
+http_response_code(200);
+
 ?>
---
+++ getQuestion.php
@@ -0,0 +1,9 @@
+<?php
+
+include_once 'connect.php';
+
+header("Access-Control-Allow-Origin: *");
+
+if (iss)
+
+?>
