9031dd5663e20eb702d112cd05059caaf2691d69
Elodie Morin <Elodie.morin1298@gmail.com>
2019-05-23 09:04:58 +0200

    small changes

--- addPerson.php
+++ addPerson.php
@@ -9,4 +9,10 @@
 
     $query = "INSERT INTO user (user_gender, user_age, user_activity) VALUES ('".$gender."', '".$age."', '".$activity."')";
     $Connect->query($query);
+
+	$query = "SELECT user_id FROM user ORDER BY user_id DESC LIMIT 1;";
+	$_SESSION['user_id'] = $Connect->query($query);
+
+
+	http_response_code(200);
 ?>
