7f034b67e1dc978a159e291428c96e23815d4c3a
Axel Maczkowiak <axel.maczkowiak@live.fr>
2019-05-23 14:46:17 +0200

    Test

--- getQuestion.php
+++ getQuestion.php
@@ -5,13 +5,14 @@ include_once 'connect.php';
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 
-if (issest($_GET['id']) || !empty($_GET['id'])){
+if (isset($_GET['id']) || !empty($_GET['id'])){
     $id = htmlentities($_GET['id']);
     if ($id > 0 && $id < 23) {
         $query = "SELECT * FROM questions WHERE question_id = ".$id.";";
         $res = mysqli_query($Connect,$query);
         $data = mysqli_fetch_row ($res);
-        echo json_encode($data);
+
+        echo $data["audio_id"];
     } else {
         http_response_code(404);
     }
