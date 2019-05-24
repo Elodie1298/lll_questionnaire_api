5bcf2c3bcf739bed73f9a22bac88122fde577554
Axel Maczkowiak <axel.maczkowiak@live.fr>
2019-05-24 16:20:15 +0200

    Added logs

--- sendQuestion.php
+++ sendQuestion.php
@@ -16,10 +16,25 @@ if (isset($_POST["user_id"]) && !empty($_POST["user_id"]) && isset($_POST["quest
     $ansinttemp = htmlentities($_POST["answer_interface_template"]);
     $anstemp = htmlentities($_POST["answer_template"]);
 
+    error_log("Writing in DB");
+
     $sql = 'INSERT INTO `answers` (`user_id`, `question_id`, `answer_text`, `answer_time`, `answer_extra_log`, `answer_interface`, `answer_interface_template`, `answer_template`) VALUES ("'.$uuid.'", '.$qid.', "'.$anstxt.'", '.$anstime.', "'.$anslog.'", "'.$ansint.'", "'.$ansinttemp.'", "'.$anstemp.'")';
     mysqli_query($Connect,$sql);
     http_response_code(200);
 } else {
+    $uuid = htmlentities($_POST["user_id"]);
+    $qid = htmlentities($_POST["question_id"]);
+    $anstxt = htmlentities($_POST["answer_text"]);
+    $anstime = htmlentities($_POST["answer_time"]);
+    $anslog = htmlentities($_POST["answer_extra_log"]);
+    $ansint = htmlentities($_POST["answer_interface"]);
+    $ansinttemp = htmlentities($_POST["answer_interface_template"]);
+    $anstemp = htmlentities($_POST["answer_template"]);
+
+    error_log("Error with some values :)");
+    error_log("user_id/question_id/answer_text/answer_time/answer_extra_log/answer_interface/answer_interface_template/answer_template");
+    error_log($uuid."/".$qid."/".$anstxt."/".$anstime."/".$anslog."/".$ansint."/".$ansinttemp."/".$anstemp);
+
     http_response_code(400);
 }
 
