503779b6a3c0287936ef78720d5f6a5d3f74ea4d
Axel Maczkowiak <axel.maczkowiak@live.fr>
2019-05-27 13:34:56 +0200

    Fixing Accents

--- getQuestionList.php
+++ getQuestionList.php
@@ -129,8 +129,8 @@ foreach ($questions as $id) {
             $val = $logic[1];
             $carac1 = $val;
     
-            if ($carac1 == "FR") $carac1 = "Fran&ccedil;ais";
-            if ($carac1 == "EN") $carac1 = "Anglais";
+            if ($carac1 == "FR") $carac1 = html_entity_decode("Fran&ccedil;ais");
+            if ($carac1 == "EN") $carac1 = html_entity_decode("Anglais");
         } else {
             $logic = explode('!',$carac1);
             $operator = "!";
@@ -138,8 +138,8 @@ foreach ($questions as $id) {
             $val = $logic[1];
             $carac1 = $val;
     
-            if ($carac1 == "EN") $carac1 = "Fran&ccedil;ais";
-            if ($carac1 == "FR") $carac1 = "Anglais";
+            if ($carac1 == "EN") $carac1 = html_entity_decode("Fran&ccedil;ais");
+            if ($carac1 == "FR") $carac1 = html_entity_decode("Anglais");
         }
 
         $carac1Audios = array();
@@ -238,16 +238,16 @@ foreach ($questions as $id) {
             $carac = $logic[0];
             $val = $logic[1];
             $carac2 = $val;
-            if ($carac2 == "FR") $carac2 = "Fran&ccedil;ais";
-            if ($carac2 == "EN") $carac2 = "Anglais";
+            if ($carac2 == "FR") $carac2 = html_entity_decode("Fran&ccedil;ais");
+            if ($carac2 == "EN") $carac2 = html_entity_decode("Anglais");
         } else {
             $logic = explode('!',$carac2);
             $operator = "!";
             $carac = $logic[0];
             $val = $logic[1];
             $carac2 = $val;
-            if ($carac2 == "EN") $carac2 = "Fran&ccedil;ais";
-            if ($carac2 == "FR") $carac2 = "Anglais";
+            if ($carac2 == "EN") $carac2 = html_entity_decode("Fran&ccedil;ais");
+            if ($carac2 == "FR") $carac2 = html_entity_decode("Anglais");
         }
         $carac2Audios = array();
 
